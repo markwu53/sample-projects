0e72e844523ca0cbd0415d04b40d622b258bebdf
Mark Wu <mang.wu@gmail.com>
2015-11-15 12:26:15 -0500

    Revert "hbase scan get put"
    This reverts commit 138a1e0c2a8b058e6c978f746d759c3d5b6e80be.

--- hadoop-maven/src/main/java/com/markwu/hadoop/HbaseTest.java
+++ hadoop-maven/src/main/java/com/markwu/hadoop/HbaseTest.java
@@ -6,11 +6,6 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HConnection;
-import org.apache.hadoop.hbase.client.HConnectionManager;
-import org.apache.hadoop.hbase.client.HTableInterface;
-import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
@@ -20,115 +15,30 @@ import org.apache.hadoop.hbase.util.Bytes;
 public class HbaseTest {
 
         public static void main(String[] args) throws Exception {
-                new HbaseTest().go();
-        }
-        
-        public void go() throws Exception {
-                test2();
-        }
-  
-        public void test1() throws Exception {
                 //Configuration conf = new Configuration();
                 Configuration conf = HBaseConfiguration.create();
-                conf.set("zookeeper.znode.parent", "/hbase");
-                //Map<String, String> ordered = new TreeMap<String, String>();
-                //for (Entry<String, String> entry: conf) ordered.put(entry.getKey(), entry.getValue());
-                //for (Entry<String, String> entry: ordered.entrySet()) System.out.println(String.format("%s=%s", entry.getKey(), entry.getValue()));
-                Connection hbaseConnection = ConnectionFactory.createConnection(conf);
-                HConnection hc = HConnectionManager.createConnection(conf);
-                System.out.println("connected");
-                HTableInterface table = hc.getTable("iemployee");
-                //Table table = hbaseConnection.getTable(TableName.valueOf("iemployee"));
-                Scan s = new Scan();
-                s.addColumn(Bytes.toBytes("insurance"), Bytes.toBytes("dental"));
-                ResultScanner resultScanner = table.getScanner(s);
-                for (Result result: resultScanner) {
-                        //System.out.println(result.toString());
-                        for (Cell cell: result.listCells()) {
-                                String rowkey = Bytes.toString(cell.getRow());
-                                String rowkey2 = Bytes.toString(cell.getRowArray());
-                                String value = Bytes.toString(cell.getValue());
-                                String value2 = Bytes.toString(cell.getValueArray());
-                                System.out.println(String.format("rowkey=%s|value=%s", rowkey, value));
-                                //System.out.println(String.format("rowkey=%s|value=%s", rowkey2, value2));
-                        }
-                        //System.out.println("row done");
-                }
-                resultScanner.close();
-                table.close();
-                hbaseConnection.close();
-        }
-
-        public void test2() throws Exception {
-                //Configuration conf = new Configuration();
-                Configuration conf = HBaseConfiguration.create();
-                conf.set("zookeeper.znode.parent", "/hbase");
                 //Map<String, String> ordered = new TreeMap<String, String>();
                 //for (Entry<String, String> entry: conf) ordered.put(entry.getKey(), entry.getValue());
                 //for (Entry<String, String> entry: ordered.entrySet()) System.out.println(String.format("%s=%s", entry.getKey(), entry.getValue()));
                 Connection hbaseConnection = ConnectionFactory.createConnection(conf);
                 System.out.println("connected");
                 Table table = hbaseConnection.getTable(TableName.valueOf("iemployee"));
-
-                {
-                        Scan s = new Scan();
-                        s.addColumn(Bytes.toBytes("insurance"), Bytes.toBytes("dental"));
-                        ResultScanner resultScanner = table.getScanner(s);
-                        for (Result result : resultScanner) {
-                                // System.out.println(result.toString());
-                                for (Cell cell : result.listCells()) {
-                                        String rowkey = Bytes.toString(cell.getRow());
-                                        String rowkey2 = Bytes.toString(cell.getRowArray());
-                                        String value = Bytes.toString(cell.getValue());
-                                        String value2 = Bytes.toString(cell.getValueArray());
-                                        System.out.println(String.format("rowkey=%s|value=%s", rowkey, value));
-                                        // System.out.println(String.format("rowkey=%s|value=%s",
-                                        // rowkey2, value2));
-                                }
-                                // System.out.println("row done");
-                        }
-                        resultScanner.close();
-                }
- 
-                {
-                        Get get = new Get(Bytes.toBytes("another"));
-                        Result result = table.get(get);
+                Scan s = new Scan();
+                s.addColumn(Bytes.toBytes("insurance"), Bytes.toBytes("dental"));
+                ResultScanner resultScanner = table.getScanner(s);
+                for (Result result: resultScanner) {
+                        System.out.println(result.toString());
                         for (Cell cell: result.listCells()) {
                                 String rowkey = Bytes.toString(cell.getRow());
                                 String rowkey2 = Bytes.toString(cell.getRowArray());
                                 String value = Bytes.toString(cell.getValue());
                                 String value2 = Bytes.toString(cell.getValueArray());
                                 System.out.println(String.format("rowkey=%s|value=%s", rowkey, value));
-                                //System.out.println(String.format("rowkey=%s|value=%s", rowkey2, value2));
+                                System.out.println(String.format("rowkey=%s|value=%s", rowkey2, value2));
                         }
+                        System.out.println("row done");
                 }
-
-                {
-                        Put put = new Put(Bytes.toBytes("1234"));
-                        put.addColumn(Bytes.toBytes("insurance"), Bytes.toBytes("dental"), Bytes.toBytes("mydental"));
-                        table.put(put);
-                }
-
-                {
-                        Scan s = new Scan();
-                        s.addColumn(Bytes.toBytes("insurance"), Bytes.toBytes("dental"));
-                        ResultScanner resultScanner = table.getScanner(s);
-                        for (Result result : resultScanner) {
-                                // System.out.println(result.toString());
-                                for (Cell cell : result.listCells()) {
-                                        String rowkey = Bytes.toString(cell.getRow());
-                                        String rowkey2 = Bytes.toString(cell.getRowArray());
-                                        String value = Bytes.toString(cell.getValue());
-                                        String value2 = Bytes.toString(cell.getValueArray());
-                                        System.out.println(String.format("rowkey=%s|value=%s", rowkey, value));
-                                        // System.out.println(String.format("rowkey=%s|value=%s",
-                                        // rowkey2, value2));
-                                }
-                                // System.out.println("row done");
-                        }
-                        resultScanner.close();
-                }
-
+                resultScanner.close();
                 table.close();
                 hbaseConnection.close();
         }

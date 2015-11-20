6f54b18385c063e9d15badb8433a3ca3af5d7a9e
Mark Wu <mang.wu@gmail.com>
2015-11-19 19:34:26 -0500

    use bean

---
+++ spring-jdbc/src/main/java/com/markwu/spring/jdbc/ConnectionStrings.java
@@ -0,0 +1,8 @@
+package com.markwu.spring.jdbc;
+
+public class ConnectionStrings {
+
+        public static final String mysql_wufamily = "jdbc:mysql://192.168.1.106:3306/information_schema";
+        public static final String mysql_hdp_sandbox = "jdbc:mysql://sandbox.hortonworks.com:3306/information_schema";
+
+}
--- spring-jdbc/src/main/java/com/markwu/spring/jdbc/JdbcExample.java
+++ spring-jdbc/src/main/java/com/markwu/spring/jdbc/JdbcExample.java
@@ -1,43 +1,70 @@
 package com.markwu.spring.jdbc;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
-public class JdbcExample {
+public class JdbcExample implements Runnable {
+
+        private static AnnotationConfigApplicationContext appContext;
 
         public static void main(String[] args) throws Exception {
-                AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(JdbcExample.class);
-                Connection conn = appContext.getBean("mysqlDataSource", DataSource.class).getConnection();
-                String sql = "select * from TABLE_PARAMS";
-                PreparedStatement ps = conn.prepareStatement(sql);
-                ResultSet rs = ps.executeQuery();
-                while (rs.next()) {
-                        System.out.println(String.format("%s|%s|%s", rs.getString(1), rs.getString(2), rs.getString(3)));
-                }
-                rs.close();
-                ps.close();
-                conn.close();
+                appContext = new AnnotationConfigApplicationContext(JdbcExample.class);
+                appContext.getBean(JdbcExample.class).run();
                 appContext.close();
         }
 
+        public void run() {
+                try {
+                        go();
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+        }
+
         @Bean
-        public DataSource mysqlDataSource() {
-                String url = "jdbc:mysql://sandbox.hortonworks.com:3306/hive";
-                String user = "root";
-                String pass = "hadoop";
-                //DriverManagerDataSource dataSource = new DriverManagerDataSource(url, user, pass);
+        public DataSource mysqlWufamilyDataSource() {
                 BasicDataSource dataSource = new BasicDataSource();
-                dataSource.setUrl(url);
-                dataSource.setUsername(user);
-                dataSource.setPassword(pass);
+                dataSource.setUrl(ConnectionStrings.mysql_wufamily);
+                dataSource.setUsername("dbuser");
+                dataSource.setPassword("12345678");
                 return dataSource;
+        }
+
+        @Bean
+        public DataSource mysqlSandboxDataSource() {
+                BasicDataSource dataSource = new BasicDataSource();
+                dataSource.setUrl(ConnectionStrings.mysql_hdp_sandbox);
+                dataSource.setUsername("hive");
+                dataSource.setPassword("hive");
+                return dataSource;
+        }
+
+        public void go() throws Exception {
+                //DataSource dataSource = appContext.getBean("mysqlWufamilyDataSource", DataSource.class);
+                DataSource dataSource = appContext.getBean("mysqlSandboxDataSource", DataSource.class);
+                JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+                String sql = "select * from tables";
+                List<String> rows = jdbc.query(sql, new RowMapper<String>() {
+                        public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                                int colCount = rs.getMetaData().getColumnCount();
+                                StringBuilder row = new StringBuilder();
+                                for (int col = 1; col <= colCount; col ++) {
+                                        row.append(rs.getString(col));
+                                        row.append(" | ");
+                                }
+                                return row.toString();
+                        }
+                });
+                for (String row: rows) System.out.println(row);
         }
 
 }

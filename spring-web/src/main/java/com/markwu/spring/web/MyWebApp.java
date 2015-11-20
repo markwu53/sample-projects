af70317fb87455c0d1284f60d5682c046abb7e98
markwu53 <markwu53@users.noreply.github.com>
2015-11-20 15:50:51 -0500

    Update MyWebApp.java

--- spring-web/src/main/java/com/markwu/spring/web/MyWebApp.java
+++ spring-web/src/main/java/com/markwu/spring/web/MyWebApp.java
@@ -1,30 +1,33 @@
 package com.markwu.spring.web;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 import javax.sql.DataSource;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@Configuration
 @EnableWebMvc
 @ComponentScan("com.markwu.spring.web")
-public class MyWebApp extends WebMvcConfigurerAdapter implements WebApplicationInitializer {
+public class MyWebApp implements WebApplicationInitializer {
 
         @Override
-        public void onStartup(ServletContext container) {
-                AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-                appContext.register(MyWebApp.class);
-                container.addListener(new ContextLoaderListener(appContext));
-                Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(appContext));
+        public void onStartup(ServletContext container) throws ServletException {
+                AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+                context.register(MyWebApp.class);
+                container.addListener(new ContextLoaderListener(context));
+                Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(context));
                 dispatcher.setLoadOnStartup(1);
                 dispatcher.addMapping("/");
         }
@@ -36,16 +39,47 @@ public class MyWebApp extends WebMvcConfigurerAdapter implements WebApplicationI
 
         @Bean
         public DataSource mysqlDataSource() {
-                try {
-                        Class.forName("com.mysql.jdbc.Driver");
-                } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                }
                 BasicDataSource dataSource = new BasicDataSource();
-                dataSource.setUrl("jdbc:mysql://sandbox.hortonworks.com:3306/information_schema");
+                dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+                dataSource.setUrl(ConnectionStrings.MYSQL_SANDBOX);
                 dataSource.setUsername("hive");
                 dataSource.setPassword("hive");
                 return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate mysqlJdbc() {
+                return new JdbcTemplate(mysqlDataSource());
+        }
+
+        @Bean
+        public DataSource fiopsDataSource() {
+                BasicDataSource dataSource = new BasicDataSource();
+                dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+                dataSource.setUrl(ConnectionStrings.ORACLE_FIOPS);
+                dataSource.setUsername("buys");
+                dataSource.setPassword("dataownr");
+                return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate fiopsJdbc() {
+                return new JdbcTemplate(fiopsDataSource());
+        }
+
+        @Bean
+        public DataSource phoenixDataSource() {
+                BasicDataSource dataSource = new BasicDataSource();
+                dataSource.setDriverClassName("org.apache.phoenix.jdbc.PhoenixDriver");
+                dataSource.setUrl(ConnectionStrings.PHOENIX);
+                //dataSource.setUsername("buys");
+                //dataSource.setPassword("dataownr");
+                return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate phoenixJdbc() {
+                return new JdbcTemplate(phoenixDataSource());
         }
 
 }

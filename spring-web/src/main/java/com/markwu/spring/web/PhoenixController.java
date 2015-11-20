76856bf59c18cbdfe89bbf27ae5f56b9d63d4787
markwu53 <markwu53@users.noreply.github.com>
2015-11-20 15:48:31 -0500

    Create PhoenixController.java

---
+++ spring-web/src/main/java/com/markwu/spring/web/PhoenixController.java
@@ -0,0 +1,43 @@
+package com.markwu.spring.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PhoenixController {
+
+        @Autowired @Qualifier("phoenixJdbc") private JdbcTemplate jdbc;
+        @Autowired private Utils utils;
+
+        @RequestMapping(value = "/phoenix/table", method = RequestMethod.GET)
+        public String schemas(Model model) {
+                //String sql = String.format("select table_schem, table_name from system.catalog");
+                String sql = "!tables";
+                List<String> header = new ArrayList<String>();
+                List<List<String>> rows = utils.myQuery(jdbc, sql, header);
+                System.out.println(header);
+                for (List<String> row: rows) System.out.println(row);
+                return "/jsp/tables.jsp";
+        }
+
+        @RequestMapping(value = "/phoenix/table/{table}", method = RequestMethod.GET)
+        public String tableData(@PathVariable("schema") String schema, @PathVariable("table") String table, Model model) {
+                String sql = String.format("select * from %s.%s", schema, table);
+                List<String> header = new ArrayList<String>();
+                List<List<String>> rows = utils.myQuery(jdbc, sql, header);
+                model
+                .addAttribute("schema", schema).addAttribute("table", table)
+                .addAttribute("tableHeader", header).addAttribute("tableRows", rows);
+                return "/jsp/tabledata.jsp";
+        }
+
+}

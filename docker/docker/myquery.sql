f445f5e1925a6cd449a94d9fbf041f08ad47e7e2
Mark Wu <mang.wu@gmail.com>
2016-07-30 12:18:27 -0400

    new

--- docker/docker/myquery.sql
+++ docker/docker/myquery.sql
@@ -8,3 +8,4 @@ select image_id from docker_layers group by image_id having count(*) > 10
 --and image not like '%:dependency'
 select * from docker_layers where image_id = '4dcab49015d47e8f300ec33400a02cebc7b54cadd09c37e49eccbc655279da90'
 select * from docker_layers where image_id = '5f70bf18a086007016e948b04aed3b82103a36bea41755b6cddfaf10ace3c6ef'
+
---
+++ docker/docker/test-mysql.sql
@@ -0,0 +1,5 @@
+create schema test;
+use test;
+create table person (name varchar(100), age int);
+insert into person values ('Mark', 46);
+select * from person;

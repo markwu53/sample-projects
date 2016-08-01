bbdbf578d5767b3453a0c812b0736f7083be60ea
T162880 <T162880@MMSURMAWUW8.mmi.mig.corp>
2016-08-01 16:41:53 -0400

    pand04

---
+++ conda-python/pand04.py
@@ -0,0 +1,15 @@
+import pandas
+
+data = pandas.read_csv("AirQualityUCI.csv", sep=";")
+#data = data[data.columns[range(6)]]
+data = data[data.columns[[1,3,5,7,9]]]
+#print(data.ix[[1,3,5]])
+#print(data.ix[range(5,9)])
+#print(data[data.index.isin(range(5,9))])
+data = data.ix[range(5,9)]
+data.index = ["A", "B", "C", "D"]
+#print(data.ix[0:3])
+#print(data.ix[range(0,3)])
+#print(data.iloc[0:3])
+#print(data.ix["A":"C"])
+#data2 = data.head(10); print(data2.ix[(data2["NO2(GT)"].astype(float)-97).abs().argsort()])

7ee7318400003fccb1118dc198d35a29a6ff0fe4
Mark Wu <mang.wu@gmail.com>
2016-08-03 00:00:28 -0400

    plot

--- conda-python/plot02.py
+++ conda-python/plot02.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-#import matplotlib
-#matplotlib.style.use("ggplot")
+import matplotlib.style
+matplotlib.style.use("ggplot")
 
 iris = pd.read_csv("iris.data")
 iris.columns=["SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Name"]
-print(iris.head())
-
-(iris.query('SepalLength > 5')
-    .assign(SepalRatio = lambda x: x.SepalWidth / x.SepalLength,
-        PetalRatio = lambda x: x.PetalWidth / x.PetalLength)
-    .plot(kind='scatter', x='SepalRatio', y='PetalRatio'))
-plt.show()
+iris = iris[iris["SepalLength"] > 5]
+iris["SepalRatio"] = iris.SepalWidth / iris.SepalLength
+iris["PetalRatio"] = iris.PetalWidth / iris.PetalLength
+#iris.plot(kind="scatter", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="line", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="bar", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="barh", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="hist", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="box", x="SepalRatio", y="PetalRatio"); plt.show()
+iris.plot(kind="kde", figsize=(10,8), x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="area", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="pie", x="SepalRatio", y="PetalRatio"); plt.show()
+#iris.plot(kind="hexbin", figsize=(10,8), x="SepalRatio", y="PetalRatio"); plt.show()
